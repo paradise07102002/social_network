b1586c184955273ddf9538786e8c92e91a727159
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-29 13:02:46 +0700

    Feat Main: Using SplashScreen()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_network/presentation/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
