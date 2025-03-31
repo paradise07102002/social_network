6608b033a26b9b564dc7fba7ad9a6ee47eaa5dea
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-31 14:57:55 +0700

    Feat main: Add SignupController

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_network/presentation/screens/auth/signup/signup_controller.dart';
 import 'package:social_network/presentation/screens/splash/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SignupController())
+      ],
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
